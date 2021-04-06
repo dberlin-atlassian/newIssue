d4d81f4b1cd947e7731aba0885e6129a25a34e15
dberlin-atlassian <82062812+dberlin-atlassian@users.noreply.github.com>
2021-04-06 16:50:21 -0500

    add the file
    it's the whole thing. user hardcoded, no error handling, no help.  v0000.000001

---
+++ newIssue.sh
@@ -0,0 +1,6 @@
+mkdir /Users/dberlin/Desktop/$1
+touch /Users/dberlin/Desktop/$1/notes$1.txt
+open /Users/dberlin/Desktop/$1/notes$1.txt
+touch /Users/dberlin/Desktop/$1/bounce1.txt
+open /Users/dberlin/Desktop/$1/bounce1.txt
+
