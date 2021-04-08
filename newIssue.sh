e219f603a0cc3afbc50b6607f4795cfcf1944cbd
Daniel C Berlin <dberlin@atlassian.com>
2021-04-08 12:59:35 -0500

    Added usage hint and user-agnostic filepaths

--- newIssue.sh
+++ newIssue.sh
@@ -1,6 +1,13 @@
-mkdir /Users/dberlin/Desktop/$1
-touch /Users/dberlin/Desktop/$1/notes$1.txt
-open /Users/dberlin/Desktop/$1/notes$1.txt
-touch /Users/dberlin/Desktop/$1/bounce1.txt
-open /Users/dberlin/Desktop/$1/bounce1.txt
+if [ -z $1 ]
+then echo "expected usage is 'sh newIssue.sh PSSRV-123456'"
+echo "exiting"
+exit
+else echo "initializing $1..."
+fi
+
+mkdir ~/Desktop/$1
+touch ~/Desktop/$1/notes$1.txt
+open ~/Desktop/$1/notes$1.txt
+touch ~/Desktop/$1/bounce1.txt
+open ~/Desktop/$1/bounce1.txt
 
